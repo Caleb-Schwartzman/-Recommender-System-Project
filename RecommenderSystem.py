bd5e7b56071e15e0be45a6fab5e25a68359ad433
brandont28 <brandont@terpmail.umd.edu>
2023-04-17 19:20:19 -0400

    Parse Args Function
    Added the parse args function that allows the code to be ran through the terminal by passing in the dataset csv file as an argument

--- RecommenderSystem.py
+++ RecommenderSystem.py
@@ -28,5 +28,22 @@ class Reccomendation:
 
 
 
-
+def parse_args(args_list):
+    """
+    Takes a string from the command prompt and passes them as an argument. Allows program
+    to be ran from the terminal
+    
+    Args:
+        args_list (list) - the list of string(s) from the command prompt
+        
+    Returns:
+        args (ArgumentParser)
+    """
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('csv_file', type = str, help = 'The path to the dataset csv file')
+    
+    args = parser.parse_args(args_list)
+    
+    return args
 
